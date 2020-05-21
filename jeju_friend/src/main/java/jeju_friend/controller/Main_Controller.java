aa9a0a26082f9eb68ad7ebc92d0cf2d38f3d3b29
LireEruel <jsh001505@gmail.com>
2020-05-21 19:12:55 +0900

    메인 뷰 컨트롤러 추가

---
+++ jeju_friend/src/main/java/jeju_friend/controller/Main_Controller.java
@@ -0,0 +1,78 @@
+package jeju_friend.controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class Main_Controller {
+
+    @FXML
+    private Label searchLabel;
+    @FXML
+    private TextField searchField;
+    @FXML
+    private Button addTravelBtn;
+    @FXML
+    private Button logoutBtn;
+    // 이벤트 핸들러
+
+    @FXML
+    public void searchField_Typed(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER || event.getCharacter().equals("\r")) {
+            // 검색창으로
+        }
+        searchLabel.setVisible(false);
+    }
+
+    @FXML
+    public void searchField_Clicked(MouseEvent event) {
+        searchLabel.setVisible(false);
+    }
+
+    @FXML
+    public void addTravelBtn_Actioned() throws IOException {
+        moveToAddTrevel();
+    }
+
+    @FXML
+    public void logoutBtn_Actioned() throws IOException {
+        moveToLogin();
+    }
+
+    // 로직
+
+    public void moveToSearch() {
+
+    }
+
+    public void moveToLogin() throws IOException
+    {
+        Stage primaryStage = (Stage) logoutBtn.getScene().getWindow(); 
+        Parent root = FXMLLoader.load(getClass().getResource("/jeju_friend/Login.fxml"));
+        Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+        primaryStage.show();   
+    }
+    public void moveToAddTrevel() throws IOException
+    {
+        Stage primaryStage = (Stage) addTravelBtn.getScene().getWindow(); 
+        Parent root = FXMLLoader.load(getClass().getResource("/jeju_friend/AddTravel.fxml"));
+        Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+        primaryStage.show(); 
+    }
+
+
+}
